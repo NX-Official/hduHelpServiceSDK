552c552f4d521d19ec1d817ec0ce8fdb123dc9b1
NX <976180942@qq.com>
2022-10-24 17:00:16 +0800

    feat():获取token与学生信息

---
+++ hduHelpSericeDTO.go
@@ -0,0 +1,33 @@
+package hduHelpService
+
+type HduhelpBaseResponse struct {
+	Cache bool   `json:"cache"`
+	Error int    `json:"error"`
+	Msg   string `json:"msg"`
+}
+
+type GetStudentInfoResponse struct {
+	HduhelpBaseResponse
+	Data struct {
+		ClassId     string `json:"classId"`
+		MajorId     string `json:"majorId"`
+		MajorName   string `json:"majorName"`
+		StaffId     string `json:"staffId"`
+		StaffName   string `json:"staffName"`
+		TeacherId   string `json:"teacherId"`
+		TeacherName string `json:"teacherName"`
+		UnitId      string `json:"unitId"`
+		UnitName    string `json:"unitName"`
+	} `json:"data"`
+}
+
+type GetTokenResponse struct {
+	HduhelpBaseResponse
+	Data struct {
+		AccessToken        string `json:"access_token,omitempty"`
+		AccessTokenExpire  int64  `json:"access_token_expire,omitempty"`
+		RefreshToken       string `json:"refresh_token,omitempty"`
+		RefreshTokenExpire int64  `json:"refresh_token_expire,omitempty"`
+		StaffId            string `json:"staff_id,omitempty"`
+	} `json:"data"`
+}
---
+++ hduHelpService.go
@@ -0,0 +1,93 @@
+package hduHelpService
+
+import (
+	"errors"
+	"fmt"
+	"github.com/parnurzeal/gorequest"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type HduHelpService struct {
+	ClientId     string
+	ClientSecret string
+	TokenInfo    struct {
+		AccessToken        string
+		AccessTokenExpire  int64
+		RefreshToken       string
+		RefreshTokenExpire int64
+		StaffId            string
+	}
+}
+
+func New(clientId, clientSecret string, token ...string) *HduHelpService {
+	var hduHelpService = &HduHelpService{
+		ClientId:     clientId,
+		ClientSecret: clientSecret,
+	}
+	if len(token) != 0 {
+		hduHelpService.TokenInfo.AccessToken = token[0]
+	}
+	return hduHelpService
+}
+
+// GetAndSaveToken 从 api.hduhelp.com 获取 token
+func (s *HduHelpService) GetAndSaveToken(code, state string) error {
+	query := make(url.Values)
+	query.Add("client_id", s.ClientId)
+	query.Add("client_secret", s.ClientSecret)
+	query.Add("grant_type", "authorization_code")
+	query.Add("code", code)
+	query.Add("state", state)
+
+	reqUrl := url.URL{
+		Scheme:   "https",
+		Host:     "api.hduhelp.com",
+		Path:     "/oauth/token",
+		RawQuery: query.Encode(),
+	}
+
+	//fmt.Println(reqUrl.String())
+
+	res := GetTokenResponse{}
+	_, _, err := gorequest.New().
+		Get(reqUrl.String()).
+		Retry(3, time.Second, http.StatusBadRequest, http.StatusInternalServerError).
+		EndStruct(&res)
+
+	s.TokenInfo = struct {
+		AccessToken        string
+		AccessTokenExpire  int64
+		RefreshToken       string
+		RefreshTokenExpire int64
+		StaffId            string
+	}(res.Data)
+
+	if err != nil {
+		return errors.New(fmt.Sprintf("%v", err))
+	} else if res.Error != 0 {
+		return errors.New(fmt.Sprintf("%v", res.Msg))
+	} else {
+		return nil
+	}
+}
+
+// GetStudentInfo 从 SalmonBase 获取学生信息
+func (s *HduHelpService) GetStudentInfo() (GetStudentInfoResponse, error) {
+	reqUrl := url.URL{
+		Scheme: "https",
+		Host:   "api.hduhelp.com",
+		Path:   "/salmon_base/student/info",
+	}
+	res := GetStudentInfoResponse{}
+	_, _, err := gorequest.New().
+		Get(reqUrl.String()).
+		Retry(3, time.Second, http.StatusBadRequest, http.StatusInternalServerError).
+		AppendHeader("Authorization", "token "+s.TokenInfo.AccessToken).EndStruct(&res)
+	if err != nil {
+		return res, errors.New(fmt.Sprintf("%v", err))
+	} else {
+		return res, nil
+	}
+}
